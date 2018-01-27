0d79afec7fc4725f6c6257bcd9d44a208f38c424
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-01-27 13:45:37 -0600

    input manager and controller script updated for multiple controllers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public string playerNumber;
+	public float speed;
+	public Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal_P" + playerNumber);
+		float moveVertical = Input.GetAxis("Vertical_P" + playerNumber);
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f , moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+
+
+}

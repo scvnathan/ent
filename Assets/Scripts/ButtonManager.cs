ad6c552bb3cbce140edb6860234c0b50f8546feb
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-01-27 20:29:39 -0600

    number of players select added, start button loading main scene working

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+	public GameState gameState;
+	public string scene;
+	public Text PlayerBtnText;
+
+	public void NewGame() {
+		SceneManager.LoadScene (scene);
+	}
+
+	public void SetPlayers(){
+		int n = gameState.numberOfPlayers;
+		if (n == 5) {
+			gameState.numberOfPlayers = 2;
+		} else {
+			gameState.numberOfPlayers++;
+		}
+		PlayerBtnText.text = "# of Players: " + gameState.numberOfPlayers;
+	}
+
+}

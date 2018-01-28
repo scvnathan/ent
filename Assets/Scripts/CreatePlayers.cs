17effc44f4076435eb7be3f8d408bf675c0a6039
Nathan Schneirov <nathan@roomera.com>
2018-01-27 21:08:42 -0600

    all kinds of littlethings

--- Assets/Scripts/CreatePlayers.cs
+++ Assets/Scripts/CreatePlayers.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Roomera.Core;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -7,16 +8,19 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class CreatePlayers : MonoBehaviour {
 
 	public Transform CouchPlayer;
-	public Transform CouchPlayers;
-	// Use this for initialization
+	
 	void Awake () {
 		Transform player;
 		int numberOfPlayers = GameState.Instance.numberOfPlayers;
-		for (int i = 2; i < numberOfPlayers; i++) {
+		for (int i = 1; i < numberOfPlayers; i++) {
 			player = Instantiate(CouchPlayer);
-			player.SetParent (CouchPlayers);
+			player.SetParent (transform);	
 			player.gameObject.GetComponent<ThirdPersonUserControl> ().playerNumber = i;
 		}
+
+		StartCoroutine(VRDeviceBootstrap.EnableVR());
 	}
+	
+	
 
 }
---
+++ Assets/Scripts/VRDeviceBootstrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Roomera.Core {
+	public static class VRDeviceBootstrap {
+		public static IEnumerator EnableVR() {
+			foreach (var device in UnityEngine.XR.XRSettings.supportedDevices) {
+				if (!device.Equals("None")) {
+					UnityEngine.XR.XRSettings.LoadDeviceByName(device);
+					yield return null;
+					
+					if (UnityEngine.XR.XRSettings.loadedDeviceName == device) {
+						break;
+					}
+				}
+			}
+			UnityEngine.XR.XRSettings.enabled = true;
+		}
+	}
+
+}

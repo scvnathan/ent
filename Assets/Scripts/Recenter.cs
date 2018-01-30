a0bd2847c63c86adfe72e3f308523d92733e56d0
Nathan Schneirov <nathan@roomera.com>
2018-01-29 18:33:28 -0600

    added thongs recenter fixes

--- Assets/Scripts/Recenter.cs
+++ Assets/Scripts/Recenter.cs
@@ -2,36 +2,22 @@
 using UnityEngine.XR;
 
 public class Recenter : MonoBehaviour {
-	public SkinnedMeshRenderer EntMesh;
-	public Transform hmd;
+    public SkinnedMeshRenderer EntMesh;
+    public Transform hmd;
 
-	public Transform cameraPosition;
-	private Vector3 loweredCamPos;
 
-	void Start () {
-		RecenterCam();
-	}
+    void Start () {
+        RecenterPlayer();
+    }
 
-	public void RecenterCam() {
-		hmd.parent.position = cameraPosition.position - hmd.position;
-		InputTracking.Recenter();
-		
-		loweredCamPos = new Vector3(cameraPosition.position.x, 1f, cameraPosition.position.z);
-//		Vector3 v = EntMesh.bounds.center - hmd.localPosition;
-//		
-//		v.z -= EntMesh.bounds.size.z / 5f;
-//		v.y += EntMesh.bounds.size.z / 5f;
-//		transform.position = v;
-//
-//		//TODO: need to fix cam rot
-//		//transform.rotation = Quaternion.FromToRotation(EntMesh.transform.parent.forward, EntMesh.transform.parent.right);
-//		
-//		EntMesh.transform.parent.rotation = Quaternion.Euler(0f, -90f, 0f);		
-	}
+    public void RecenterPlayer() {
+        Vector3 c = EntMesh.bounds.center;
+        Vector3 v = new Vector3(c.x, c.y + EntMesh.bounds.extents.y * .5f, c.z);
 
-	public void Update() {
-		if ((loweredCamPos - new Vector3(hmd.position.x, 1f, hmd.position.z)).magnitude > 5f) {
-			RecenterCam();
-		}
-	}
+        Vector3 offset = hmd.localPosition;
+        hmd.parent.position = v - offset;
+
+        float angle = Vector3.Angle (hmd.forward, EntMesh.transform.forward);
+        hmd.parent.RotateAround (hmd.position, Vector3.up, -angle);
+    }
 }

9f95ec041baa81c9769026602ec1abee873cc3f7
Nathan <scvnathan@gmail.com>
2018-02-04 22:33:44 -0600

    added a lil intro thing

---
+++ Assets/Intro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Asyncoroutine;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+	public List<CanvasGroup> textIntro = new List<CanvasGroup>();
+
+	async void Start() {
+		await new WaitForSeconds(3f);
+		
+		var seq = LeanTween.sequence();
+		for (int i = 0; i < textIntro.Count; i++) {
+			seq.append(LeanTween.alphaCanvas(textIntro[i], 1f, 2f).setDelay(2.5f).setEaseInQuad());
+		}
+
+		seq.append(() => {
+			for (int i = 0; i < textIntro.Count; i++) {
+				LeanTween.moveY(textIntro[i].GetComponent<RectTransform>(), 1000f, 5f).setEaseInQuad();
+				LeanTween.alphaCanvas(textIntro[i], 0f, 3f);
+			}
+		});
+	}
+}
